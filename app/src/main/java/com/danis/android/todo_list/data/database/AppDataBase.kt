641ab2673cb066d5784d97b1ea70a61a9b65d06d
Danis <danis.ru2002@mail.ru>
2023-01-04 14:25:08 +0300

    Version 2.2

--- app/src/main/java/com/danis/android/todo_list/data/database/AppDataBase.kt
+++ app/src/main/java/com/danis/android/todo_list/data/database/AppDataBase.kt
@@ -36,8 +36,8 @@ abstract class AppDataBase : RoomDatabase() {
 val migration_1_2 = object:Migration(1,2){
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE CaseTODO RENAME TO CaseTODODbModel")
+        database.execSQL("ALTER TABLE CaseTODODbModel ADD COLUMN notificationId INTEGER DEFAULT NULL")
         database.execSQL("ALTER TABLE CaseNotes RENAME TO CaseNoteDbModel")
-        database.execSQL("ALTER TABLE CaseTODODbModel ADD COLUMN notificationId INTEGER")
     }
 
 }
